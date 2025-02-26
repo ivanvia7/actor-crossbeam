79f446337f9fd3a65280ee5e7c11a15a6aec6e94
Ivan Vasilev <132016310+ivanvia7@users.noreply.github.com>
2025-02-26 01:19:37 +0100

    added one more router

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Dataset, createPlaywrightRouter } from "crawlee";
 import { BASE_URL, labels, USER_EMAIL, USER_PWD } from "./consts.js";
+import { CompanyProfile } from "./types.js";
 
 export const router = createPlaywrightRouter();
 
@@ -67,16 +68,48 @@ router.addHandler(labels.LOGIN, async ({ enqueueLinks, page, log }) => {
     });
 });
 
-router.addHandler(labels.PROFILE, async ({ request, page, log }) => {
-    // some code here
+router.addHandler<CompanyProfile>(labels.PROFILE, async ({ page, log }) => {
+    //Constants for the locators
+    const CONTACT_ID_LABEL = "text=Contact ID";
+    const COMPANY_ID_LABEL = "text=Primary Associated Company ID";
+    const EMAIL_LABEL = "text=Email";
+    const WEB_URL = "text=Website URL";
+    const COMPANY_NAME_LABEL = "text=Crossbeam - Company name";
+
+    //matching the label with the field label in the table
+    async function getFieldValue(labelLocator: string) {
+        const labelElement = await page.locator(labelLocator);
+        const valueElement = labelElement.locator(
+            "xpath=following-sibling::div/span"
+        );
+
+        if ((await valueElement.count()) > 0) {
+            return await valueElement.innerText();
+        }
+
+        log.info("Cannot find the information for labelLocator", {
+            labelLocator,
+        });
+
+        return null;
+    }
+
+    const contactId = await getFieldValue(CONTACT_ID_LABEL);
+    const email = await getFieldValue(EMAIL_LABEL);
+    const company_name = await getFieldValue(COMPANY_NAME_LABEL);
+    const contactWeb = await getFieldValue(WEB_URL);
+    const companyId = await getFieldValue(COMPANY_ID_LABEL);
+
+    await Dataset.pushData({
+        contactId: contactId || "N/A",
+        email: email || "N/A",
+        company_name: company_name || "N/A",
+        contactWeb: contactWeb || "N/A",
+        companyId: companyId || "N/A",
+    });
 });
 
 router.addHandler("detail", async ({ request, page, log }) => {
     const title = await page.title();
     log.info(`${title}`, { url: request.loadedUrl });
-
-    await Dataset.pushData({
-        url: request.loadedUrl,
-        title,
-    });
 });
